68ddc0ecff0c8e6ffbcfb2d6ef23a8c03467be54
Ruskin Kollin <ruskin.kollin@sa-tu.com>
2022-12-15 10:33:42 +0200

    To remote branch test2remote

--- test-branch/src/App.js
+++ test-branch/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>On the branch2 branch</p>
+        <p>Test 2 remote</p>
         <a
           className="App-link"
           href="https://reactjs.org"
