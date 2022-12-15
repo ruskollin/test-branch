3c20e9d1ee79d30284231b90639b2f4511ab6832
Ruskin Kollin <ruskin.kollin@sa-tu.com>
2022-12-15 10:01:35 +0200

    Second commit to test1 branch

--- test-branch/src/App.js
+++ test-branch/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Branch Master</p>
+        <p>On test1 branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
